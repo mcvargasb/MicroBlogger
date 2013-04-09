cbd0c8af0e6a77778198a2b11511a51d9bafe96a
Carmen Vargas <mcvargasb@gmail.com>
2013-04-09 10:47:38 +0200

    Final commit

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -1,11 +1,17 @@
 require 'jumpstart_auth'
+require 'bitly'
+require 'klout'
+
 
 class MicroBlogger
   attr_reader :client
 
   def initialize
     puts "Initializing"
-    @client = JumpstartAuth.twitter
+   @client = JumpstartAuth.twitter
+   Bitly.use_api_version_3
+   @bitly = Bitly.new('hungryacademy', 'R_430e9f62250186d2612cca76eee2dbc6') 
+   Klout.api_key = 'xu9ztgnacmjx3bu82warbr3h'
   end
 
     def run
@@ -20,7 +26,12 @@ class MicroBlogger
          when 'q' then puts "Goodbye!"
          when 't' then tweet(parts[1..-1].join(" "))
          when 'dm' then dm(parts[1], parts[2..-1].join(" "))
-      else
+         when 'spam' then spam_my_follo       
+         when 'spam' then spam_my_followers(parts[1..-1].join(" "))
+         when 'elt' then everyones_last_tweet
+         when 's' then shorten(parts[1])
+         when 'turl' then tweet parts[1..-2].join(" ") + " " +shorten(parts[-1])         when 'klout' then klout_scores 
+     else
            puts "Sorry, I don't know how to (#{command})"
       end
     end
@@ -49,8 +60,48 @@ class MicroBlogger
 
   def followers_list
    screen_names = []
-  end 
+   @client.followers.each do |follower|
+   screen_names << follower ['screen_name'] 
+   end 
+   screen_names
+end
+
+  def spam_my_followers(message)
+   followers_list.each do |follower|
+   dm(follower, message)
+   end
+  end
+
+  def everyones_last_tweet	
+   friends = @client.friends
+   friends.collect {|friend| friend.screen_name.downcase}
+   friends.sort_by! {|friend| friend.screen_name}
+
+   friends.each do |friend|
+    last_tweet = friend.status.text
+    timestamp = friend.status.created_at
+    date = timestamp.strftime("%A, %b %d")
+    puts "#{friend.screen_name} said this on #{date}:"
+    puts "#{last_tweet}"
+    end
+  end
+
+  def shorten(original_url)
+    short_url = @bitly.shorten(original_url).short_url
+    puts "Shortening this URL: #{short_url}"
+  end
+
+  def klout_scores
+   friends = friends = @client.friends.collect{|f| f.screen_name}
+   friends.each do |friend|
+   identity = Klout::Identity.find_by_screen_name(friend)
+      user = Klout::User.new(identity.id)
+      puts friend+" has "+user.score.score.round(2).to_s+" Klout score."
+     puts ""
+   end
+ end
 end
 
 blogger = MicroBlogger.new
 blogger.run
+blogger.klout_scores
